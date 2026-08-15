489afe5add00d140805dcfa05d339cae87398eb4
javijec <javijec@gmail.com>
2026-08-15 12:04:09 -0300

    chore: drop orphaned assets, utilities, scripts and stale tsbuildinfo

--- lib/utilities/league.ts
+++
@@ -1,9 +0,0 @@
-export const formatLeagueLabel = (league: string): string => {
-  const normalizedLeague = league.replace(/^(poe2|xbox|sony)\//i, "")
-
-  try {
-    return decodeURIComponent(normalizedLeague)
-  } catch {
-    return normalizedLeague.replace(/%20/g, " ")
-  }
-}
